3aef8b18ab658d0d24d2a45fd969d09bdc06a236
Tobias Engler <80252945+rustobi@users.noreply.github.com>
2024-08-19 16:09:17 +0200

    vacuum cleaner

--- tado-tool.py
+++ tado-tool.py
@@ -12,17 +12,12 @@ CITY="Krefeld"
 def get_weather(api_key):
     # OpenWeatherMap API URL
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    
-    # Vollständige URL mit API-Schlüssel und Stadtname
     complete_url = f"{base_url}appid={api_key}&q={CITY}&units=metric"
 
-    # HTTP-Anfrage an die API
     response = requests.get(complete_url)
     
-    # Antwort in JSON konvertieren
     data = response.json()
 
-    # Überprüfen, ob die Stadt gefunden wurde
     if data["cod"] != "404":
         main = data["main"]
         temperature = main["temp"]
@@ -33,13 +28,9 @@ def get_weather(api_key):
         return None
 
 def write_to_csv():
-    # Tado-Objekt erstellen
     t = Tado(TADO_USERNAME, TADO_PASSWORD)
-
-    # Zonen abrufen
     zones = t.get_zones()
 
-    # Liste zur Speicherung der Klimadaten erstellen
     climate_data = []
     datetime_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -47,17 +38,14 @@ def write_to_csv():
     current_temp = f"{current_temp:.2f}".replace('.', ',')
     current_humidity = f"{current_humidity:.2f}".replace('.', ',')
 
-    # Klimadaten für jede Zone abrufen
     for zone in zones:
         zone_id = zone['id']
         zone_name = zone['name']
         
-        # Klimadaten für die aktuelle Zone abrufen
         climate = t.get_climate(zone=zone_id)
         temperature = f"{climate['temperature']:.2f}".replace('.', ',')
         humidity = f"{climate['humidity']:.2f}".replace('.', ',')
         
-        # Daten sammeln
         climate_data.append({
             "Zone": zone_name,
             "Timestamp": datetime_now,
@@ -73,10 +61,8 @@ def write_to_csv():
             "Humidity (%)": current_humidity
         })
 
-    # Daten in einem DataFrame speichern
     df = pd.DataFrame(climate_data)
 
-    # Daten in einer CSV-Datei speichern mit UTF-8 Encoding
     csv_filename = os.path.join(Path(__file__).parent, "tado_climate_data.csv")
 
     if os.path.isfile(csv_filename):
